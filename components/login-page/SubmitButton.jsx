855dd36b88c5772c23744bffe5c73537acb9762f
amirhosseinShamsi <shamsiamir2000@gmail.com>
2023-10-09 22:02:15 +0330

    state management

--- components/login-page/SubmitButton.jsx
+++ components/login-page/SubmitButton.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import SpinnerLoading from "components/login-page/SpinnerLoading";
 
 const SubmitButton = ({ text, type, isLoading }) => {
   return (
     <>
-      <button
-        className="w-full py-3 bg-indigo-500 rounded-lg text-white"
-        type={type}
-      >
-        {isLoading ? "isLoading..." : text}
-      </button>
+      {!isLoading ? (
+        <button
+          className="w-full py-3 bg-indigo-500 rounded-lg text-white"
+          type={type}
+        >
+          {text}
+        </button>
+      ) : (
+        <SpinnerLoading />
+      )}
     </>
   );
 };
---
+++ src/app/test/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            this is dashboard page
+        </div>
+    );
+};
+
+export default Page;
